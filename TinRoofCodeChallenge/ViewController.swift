fd9375105ba337cd7ec700af7b9c2fda41000332
Gabriel Jaramillo <ing.gabrieljaramillo@gmail.com>
2018-05-22 02:58:58 -0400

    Initial Commit

---
+++ TinRoofCodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinRoofCodeChallenge
+//
+//  Created by Gabriel Perez on 5/22/18.
+//  Copyright © 2018 Gabriel Jaramillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
